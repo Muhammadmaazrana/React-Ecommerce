4b38c5408c44980708dd67d0eb2736c82750f8a3
MUHAMMAD MAAZ RANA <80382672+Muhammadmaazrana@users.noreply.github.com>
2023-05-31 13:16:11 +0500

    set API with xious

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import { useProductContext } from './context/ProductContext'
 
 function About() {
+// const myName = useContext(AppContext)
+const {myName}=useProductContext();
+
     const data={
         name:"Maaz E-commerce"
       }
   return (
     <div>
+      {myName}
       <HeroSection myData={data}/>
     </div>
   )
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { createContext, useContext, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const API = "https://api.pujakaitem.com/api/products"
+
+const AppProvider = ({children})=>{
+
+    const getProducts =async (url)=>{
+const res = await axios(url);
+const products = await res.data;
+console.log(products)
+    } 
+
+useEffect(()=>{
+    getProducts(API);
+},[]);
+
+return(
+<AppContext.Provider value={{ myName:"maaz"}}>{children}</AppContext.Provider>
+);
+
+}
+
+const useProductContext=()=>{
+return useContext(AppContext);
+}
+
+export {AppProvider,AppContext,useProductContext};
